55259d26da507b411e969ddbed4528e30485f34b
besoeasy <besoeasy@users.noreply.github.com>
2024-06-22 22:02:14 +0530

    chore: Update npm dependencies
    This commit updates the npm dependencies in the project. Specifically, it adds the `axios` package with version `1.7.2` and the `timeago.js` package with version `4.0.2`. This update ensures that the project is using the latest stable versions of these packages.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,126 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import { format } from 'timeago.js'
+
+const commits = ref([])
+const page = ref(0)
+const showloadmore = ref(true)
+
+async function fetchCommits(page) {
+  try {
+    const response = await axios.post(
+      'https://pool.albertiprotocol.org/graphql',
+      {
+        query: `query GetCommits($page: Int!, $perPage: Int!) {
+          getCommits(page: $page, perPage: $perPage) {
+            commitAt
+            data 
+            address
+            publicKey
+            signature
+            type
+            nonce
+            createdAt
+            updatedAt
+          }
+        }`,
+        variables: {
+          page: page,
+          perPage: 9
+        }
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    if (response.data.errors) {
+      console.error(response.data.errors)
+      return []
+    }
+
+    return response.data.data.getCommits
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+function scrollbind() {
+  fetchCommits(page.value).then((data) => {
+    if (data.length > 0) {
+      page.value++
+      commits.value = commits.value.concat(data)
+    } else {
+      showloadmore.value = false
+    }
+  })
+}
+
+onMounted(() => {
+  scrollbind()
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container p-10 px-0 mx-auto md:py-20 md:p-10 md:px-0">
+    <div class="grid grid-cols-1 gap-4 xl:grid-cols-3">
+      <div
+        v-for="commit in commits"
+        :key="commit.signature"
+        class="p-5 py-8 text-left transition-transform duration-300 bg-white rounded-md shadow-md cursor-pointer hover:shadow-xl hover:-translate-y-1"
+      >
+        <div class="mb-4">
+          <p class="text-xs font-bold text-gray-500">
+            {{ commit.address }} - {{ format(commit.updatedAt) }}
+          </p>
+          <h1 class="mt-2 mb-4 text-xl font-extrabold text-gray-800">
+            {{ commit.data.content || '' }}
+          </h1>
+        </div>
+
+        <div
+          v-if="commit.data.hashtags && commit.data.hashtags.length > 0"
+          class="flex flex-wrap mb-4"
+        >
+          <span
+            v-for="hashtag in commit.data.hashtags"
+            :key="hashtag"
+            class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 mb-2 px-2.5 py-0.5 rounded"
+          >
+            {{ hashtag }}
+          </span>
+        </div>
+
+        <div v-if="commit.data.attachments && commit.data.attachments.length > 0">
+          <div v-for="attachment in commit.data.attachments" :key="attachment.cid" class="mb-4">
+            <img
+              v-if="attachment.type === 'image' && attachment.cid"
+              :src="`https://ipfs.io/ipfs/${attachment.cid}`"
+              alt="Attachment Image"
+              class="w-full rounded-lg"
+            />
+            <img
+              v-if="attachment.type === 'image' && attachment.url"
+              :src="`${attachment.url}`"
+              alt="Attachment Image"
+              class="w-full rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-8 text-center" v-if="showloadmore">
+      <button
+        @click="scrollbind()"
+        class="px-4 py-2 font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow-md hover:bg-blue-600"
+      >
+        Load More
+      </button>
+    </div>
+  </div>
 </template>
