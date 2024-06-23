16d8f5f63d916f35df316c4b63b1f63b517736e8
besoeasy <besoeasy@users.noreply.github.com>
2024-06-23 13:44:06 +0530

    refactor: Update RouterLink in App.vue to point to "/timeline"
    This commit updates the RouterLink in the App.vue component to point to the "/timeline" route instead of the root route ("/"). This change ensures that the "HOME" link correctly navigates to the timeline page, providing users with a more intuitive browsing experience.

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ const isHome = computed(() => route.path === '/')
   <div class="container p-10 m-auto">
     <div class="px-5 m-auto mb-10 text-black bg-yellow-100 rounded-2xl" v-if="!isHome">
       <div class="flex items-center justify-between h-8 px-4 py-8 font-bold uppercase md:text-lg">
-        <RouterLink to="/" class="router-link-active router-link-exact-active">HOME</RouterLink>
+        <RouterLink to="/timeline" class="router-link-active router-link-exact-active">HOME</RouterLink>
         <div class="flex items-center space-x-4">
           <div class="flex items-center space-x-4 text-sm md:space-x-8">
             <a class="">*</a>
